1986ff82f998e445885edfe5d536ce926f2ea5ce
Q4ck <quckpriv@hotmail.com>
2024-10-04 00:31:16 -0500

    Mensaje descriptivo de los cambios

---
+++ juego_tiempo/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblWord = New Label()
+        lblTimer = New Label()
+        lblRound = New Label()
+        btnSubmit = New Button()
+        txtInput = New TextBox()
+        cmbDifficulty = New ComboBox()
+        btnStart = New Button()
+        SuspendLayout()
+        ' 
+        ' lblWord
+        ' 
+        lblWord.AutoSize = True
+        lblWord.Location = New Point(153, 74)
+        lblWord.Name = "lblWord"
+        lblWord.Size = New Size(53, 20)
+        lblWord.TabIndex = 0
+        lblWord.Text = "Label1"
+        ' 
+        ' lblTimer
+        ' 
+        lblTimer.AutoSize = True
+        lblTimer.Location = New Point(382, 71)
+        lblTimer.Name = "lblTimer"
+        lblTimer.Size = New Size(53, 20)
+        lblTimer.TabIndex = 1
+        lblTimer.Text = "Label2"
+        lblTimer.Visible = False
+        ' 
+        ' lblRound
+        ' 
+        lblRound.AutoSize = True
+        lblRound.Location = New Point(629, 62)
+        lblRound.Name = "lblRound"
+        lblRound.Size = New Size(53, 20)
+        lblRound.TabIndex = 2
+        lblRound.Text = "Label3"
+        ' 
+        ' btnSubmit
+        ' 
+        btnSubmit.Location = New Point(502, 232)
+        btnSubmit.Name = "btnSubmit"
+        btnSubmit.Size = New Size(94, 29)
+        btnSubmit.TabIndex = 3
+        btnSubmit.Text = "Button1"
+        btnSubmit.UseVisualStyleBackColor = True
+        ' 
+        ' txtInput
+        ' 
+        txtInput.Location = New Point(162, 223)
+        txtInput.Name = "txtInput"
+        txtInput.Size = New Size(125, 27)
+        txtInput.TabIndex = 4
+        ' 
+        ' cmbDifficulty
+        ' 
+        cmbDifficulty.FormattingEnabled = True
+        cmbDifficulty.Location = New Point(334, 139)
+        cmbDifficulty.Name = "cmbDifficulty"
+        cmbDifficulty.Size = New Size(151, 28)
+        cmbDifficulty.TabIndex = 5
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(663, 138)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(94, 29)
+        btnStart.TabIndex = 6
+        btnStart.Text = "btnStart"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnStart)
+        Controls.Add(cmbDifficulty)
+        Controls.Add(txtInput)
+        Controls.Add(btnSubmit)
+        Controls.Add(lblRound)
+        Controls.Add(lblTimer)
+        Controls.Add(lblWord)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblWord As Label
+    Friend WithEvents lblTimer As Label
+    Friend WithEvents lblRound As Label
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents cmbDifficulty As ComboBox
+    Friend WithEvents btnStart As Button
+
+End Class
---
+++ juego_tiempo/Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Forms
+
+Public Class Form1
+    Private palabras As List(Of String)
+    Private palabrasMostradas As List(Of String)
+    Private random As Random
+    Private currentRound As Integer
+    Private totalRounds As Integer
+    Private timeLeft As Integer
+    Private timer As Timer
+    Private easyTime As Integer = 15 ' Tiempo para dificultad fácil
+    Private mediumTime As Integer = 10 ' Tiempo para dificultad media
+    Private hardTime As Integer = 5 ' Tiempo para dificultad difícil
+
+    Public Sub New()
+        InitializeComponent()
+        palabras = New List(Of String) From {"amor", "libertad", "felicidad", "paz", "esperanza", "amistad", "familia", "trabajo", "casa", "viaje"}
+        palabrasMostradas = New List(Of String)
+        random = New Random()
+        currentRound = 0
+        totalRounds = 5
+        timer = New Timer()
+        timer.Interval = 1000 ' 1 segundo
+        AddHandler timer.Tick, AddressOf Timer_Tick
+
+        cmbDifficulty.Items.AddRange(New String() {"Fácil", "Medio", "Difícil"})
+        cmbDifficulty.SelectedIndex = 1 ' Medio por defecto
+        AddHandler cmbDifficulty.SelectedIndexChanged, AddressOf cmbDifficulty_SelectedIndexChanged
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs)
+        timeLeft -= 1
+        lblTimer.Text = $"Tiempo: {timeLeft}s"
+        If timeLeft <= 0 Then
+            timer.Stop()
+            MessageBox.Show("Se acabó el tiempo. Has perdido la ronda.")
+            StartNewRound()
+        End If
+    End Sub
+
+    Private Sub StartNewRound()
+        If currentRound >= totalRounds Then
+            MessageBox.Show("¡Juego completado!")
+            Me.Close()
+            Return
+        End If
+
+        currentRound += 1
+        lblRound.Text = $"Ronda: {currentRound}/{totalRounds}"
+        Dim nuevaPalabra As String
+        Do
+            nuevaPalabra = palabras(random.Next(palabras.Count))
+        Loop While palabrasMostradas.Contains(nuevaPalabra)
+        palabrasMostradas.Add(nuevaPalabra)
+        lblWord.Text = nuevaPalabra
+        txtInput.Clear()
+        SetTimeLeftBasedOnDifficulty()
+        lblTimer.Text = $"Tiempo: {timeLeft}s"
+        timer.Start()
+    End Sub
+
+    Private Sub SetTimeLeftBasedOnDifficulty()
+        Select Case cmbDifficulty.SelectedItem.ToString()
+            Case "Fácil"
+                timeLeft = easyTime
+            Case "Medio"
+                timeLeft = mediumTime
+            Case "Difícil"
+                timeLeft = hardTime
+            Case Else
+                timeLeft = mediumTime
+        End Select
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        If txtInput.Text = lblWord.Text Then
+            timer.Stop()
+            MessageBox.Show("Palabra correcta. ¡Ronda completada!")
+            StartNewRound()
+        Else
+            MessageBox.Show("Palabra incorrecta. Inténtalo de nuevo.")
+            txtInput.Clear()
+        End If
+    End Sub
+
+    Private Sub cmbDifficulty_SelectedIndexChanged(sender As Object, e As EventArgs)
+        SetTimeLeftBasedOnDifficulty()
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        cmbDifficulty.Visible = False
+        lblTimer.Visible = True
+        btnStart.Visible = False
+        currentRound = 0
+        StartNewRound()
+    End Sub
+End Class
